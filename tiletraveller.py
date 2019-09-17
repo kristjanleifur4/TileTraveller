59621686bfe5912239a636b015d8e1026ec2edf5
juliush16 <juliush16@ru.is>
2019-09-17 14:39:54 +0000

    final commit

--- tiletraveller.py
+++ tiletraveller.py
@@ -1,59 +1,53 @@
-<<<<<<< HEAD
-#lalal
-=======
-# Við skilgreinum x = 1 og y = 1
-# Við skilgreinum allar áttirnar og gerum lista yfir hvenær þú mátt fara upp,
-# hvenær má fara niður og hvenær má fara til vinstri eða hægri
-# Þá látum við N = True eða N = False eftir því hvort þú getur farið upp eða ekki, gerum þetta fyrir allar áttirnar
-# Látum forritið enda þegar x = 3 og y = 1
+#github repo: https://github.com/kristjanleifur4/TileTraveller.git
 x,y = 1,1
-while (x,y) != (3,1):
+while (x,y) != (3,1): #á meðan x,y er ekki 3,1 þá heldur programmið áfram
     a = x
     b = y
-    N = True
-    E = True
-    S = True
-    W = True
+    north = True
+    east = True
+    south = True
+    west = True
     if y == 3 or (x,y) == (2,2) or (x,y) == (3,1):
-        N = False
+        north = False
     if (x,y) != (1,2) and (x,y) != (1,3) and (x,y) != (2,3):
-        E = False
+        east = False
     if y == 1 or (x,y) == (2,3):
-        S = False
+        south = False
     if y == 1 or x == 1 or (x,y) == (3,2):
-        W = False
-    directions = ""
-    while directions == "":
-        if N == True:
-            directions += "(N)orth"
-        elif E == True:
-            directions += "(E)ast"
-        elif S == True:
-            directions += "(S)outh"
-        elif W == True:
-            directions += "(W)est"
-    if E == True and directions != "(E)ast":
-        directions += " or (E)ast"
-    if S == True and directions != "(S)outh":
-        directions += " or (S)outh"
-    if W == True and directions != "(W)est":
-        directions += " or (W)est"
-    print("You can travel: " + directions + ".")
-    command = ""
+        west = False
+
+    direction = ""
+    while direction == "": #hvaða átt notandinn fer í 
+        if north == True:
+            direction += "(N)orth"
+        elif east == True:
+            direction += "(E)ast"
+        elif south == True:
+            direction += "(S)outh"
+        elif west == True:
+            direction += "(W)est"
+    if east == True and direction != "(E)ast":
+        direction += " or (E)ast"
+    if south == True and direction != "(S)outh":
+        direction += " or (S)outh"
+    if west == True and direction != "(W)est":
+        direction += " or (W)est"
+    print("You can travel: " + direction + ".") #hvaða valmöguleikar eru í boði
+
+    directionChoice = ""
     while (x,y) == (a,b):
-        command = input("Direction: ")
-        command = command.upper()
-        if command == "N" and N == True:
+        directionChoice = input("Direction: ")
+        directionChoice = directionChoice.upper()
+        if directionChoice == "N" and north == True:
             y += 1
-        elif command == "S" and S == True:
-            y -= 1
-        elif command == "W" and W == True:
-            x -= 1
-        elif command == "E" and E == True:
+        elif directionChoice == "E" and east == True:
             x += 1
+        elif directionChoice == "S" and south == True:
+            y -= 1
+        elif directionChoice == "W" and west == True:
+            x -= 1
         else:
             print("Not a valid direction!")
-print("Victory!")
+print("Victory!") #ef það er farið á 3,1 þá prentast út victory 
     
-    
->>>>>>> bccdd4673b6b0132fb3823c646a16f713a0429ab
+
